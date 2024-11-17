e3fb90cd65972be9bba4e6a199302b5e02c50cda
firmandayat <firman.txt@gmail.com>
2024-11-17 22:43:44 +0700

    add about us

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -27,12 +27,38 @@ const Home = {
                             cumque laboriosam
                             totam, quod quibusdam laudantium corporis dolore quia obcaecati corrupti magni? Laudantium
                             quaerat iste
+                            dignissimos in repellendus est optio sed labore. Ratione, praesentium. Quam vitae
+                            unde, nostrum id quis odio nobis nisi consequuntur debitis? In architecto molestiae ipsa
+                            cumque laboriosam
+                            totam, quod quibusdam laudantium corporis dolore quia obcaecati corrupti magni? Laudantium
+                            quaerat iste
                             dignissimos in repellendus est optio sed labore. Ratione, praesentium.</p>
                     </article>
                 </div>
             </section>
           <h2 style="margin-top:50px">Restaurants</h2>
           <div id="restaurant" class="wrapper-resto"></div>
+          <section id="About Us" class="about-us">
+            <h2 tabindex="0">About Us</h2>
+            <p tabindex="0" class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente
+                quod amet cumque,
+                voluptatibus
+                earum tenetur unde eveniet quas, itaque iure culpa ex quidem commodi aperiam corrupti dolore alias
+                ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente
+                quod amet cumque,
+                voluptatibus
+                earum tenetur unde eveniet quas, itaque iure culpa ex quidem commodi aperiam corrupti dolore alias
+                ducimus? 
+            </p>
+            <div class="sosmed">
+                <a href="https://www.instagram.com/firmandyt/" target="_blank"><img src="./images/logos/ig.png"
+                        width="45px" alt="Instagram Logo"></a>
+                <a href="https://www.linkedin.com/in/firmanhidayatullah//" target="_blank"><img
+                        src="./images/logos/linkedin.png" width="45px" alt="Linkedin Logo"></a>
+                <a href="https://mailto:firman.txt@gmail.com" target="_blank"><img src="./images/logos/gm.png"
+                        width="45px" alt="Gmail Logo"></a>
+            </div>
+        </section>
         </div>          
         `;
   },
