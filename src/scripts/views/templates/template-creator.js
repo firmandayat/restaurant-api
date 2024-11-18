f7a47bfb967374749429e765e65eac3389e6a227
firmandayat <firman.txt@gmail.com>
2024-11-18 12:35:35 +0700

    font awesome

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -42,7 +42,7 @@ const createRestoDetailTemplate = (resto) => `
 
 const createLikeButtonTemplate = () => `
   <button aria-label="like this movie" id="likeButton" class="like">
-     <i class="fa fa-heart-o" aria-hidden="true"></i>
+     <i class="fa-regular fa-heart" aria-hidden="true"></i>
   </button>
 `;
 
