9a5487c6e4c84f0191519afbbdd7601a49340b5a
firmandayat <firman.txt@gmail.com>
2024-11-18 19:32:59 +0700

    skip content

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -5,26 +5,37 @@ import { createRestoItemTemplate } from '../templates/template-creator';
 const Home = {
   async render() {
     return `
-            <article>
-              <p tabindex="0" class="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
-                  tempore atque reprehenderit,
-                  ratione, illo
-                  neque est perspiciatis itaque temporibus ipsam dignissimos adipisci. Eius alias eligendi
-                  laborum facilis
-                  ipsam
-                  repellat, ipsa quasi voluptatum, esse iure ut recusandae obcaecati nemo quibusdam quod
-                  libero? Quam vitae
-                  unde, nostrum id quis odio nobis nisi consequuntur debitis? In architecto molestiae ipsa
-                  cumque laboriosam
-                  totam, quod quibusdam laudantium corporis dolore quia obcaecati corrupti magni? Laudantium
-                  quaerat iste
-                  dignissimos in repellendus est optio sed labore. Ratione, praesentium. Quam vitae
-                  unde, nostrum id quis odio nobis nisi consequuntur debitis? In architecto molestiae ipsa
-                  cumque laboriosam
-                  totam, quod quibusdam laudantium corporis dolore quia obcaecati corrupti magni? Laudantium
-                  quaerat iste
-                  dignissimos in repellendus est optio sed labore. Ratione, praesentium.</p>
-          </article>
+    <section id="Home">
+            <img class="bg-banner" src="./images/heros/hero-image_4.jpg" alt="Hero Image" />
+        </section>
+        <div class="container">
+            <section class="description">
+                <h2 tabindex="0">
+                    Description
+                </h2>
+                <div id="mainContent" class="content">
+                    <article>
+                        <p tabindex="0" class="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+                            tempore atque reprehenderit,
+                            ratione, illo
+                            neque est perspiciatis itaque temporibus ipsam dignissimos adipisci. Eius alias eligendi
+                            laborum facilis
+                            ipsam
+                            repellat, ipsa quasi voluptatum, esse iure ut recusandae obcaecati nemo quibusdam quod
+                            libero? Quam vitae
+                            unde, nostrum id quis odio nobis nisi consequuntur debitis? In architecto molestiae ipsa
+                            cumque laboriosam
+                            totam, quod quibusdam laudantium corporis dolore quia obcaecati corrupti magni? Laudantium
+                            quaerat iste
+                            dignissimos in repellendus est optio sed labore. Ratione, praesentium. Quam vitae
+                            unde, nostrum id quis odio nobis nisi consequuntur debitis? In architecto molestiae ipsa
+                            cumque laboriosam
+                            totam, quod quibusdam laudantium corporis dolore quia obcaecati corrupti magni? Laudantium
+                            quaerat iste
+                            dignissimos in repellendus est optio sed labore. Ratione, praesentium.</p>
+                    </article>
+                </div>
+            </section>
           <h2 style="margin-top:50px">Restaurants</h2>
           <div id="restaurant" class="wrapper-resto"></div>
           <section id="Contact Us" class="contact-us">
