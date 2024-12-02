34ee17960c7609c9467f50b30e87ca07b4e16590
firmandayat <firman.txt@gmail.com>
2024-12-02 10:22:32 +0700

    testing

--- tests/likeResto.test.js
+++ tests/likeResto.test.js
@@ -1,6 +1,6 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-undef */
 import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteResto from '../src/scripts/data/favorite-resto';
+
 describe('Liking A Resto', () => {
   it('should show the like button when the resto has not been liked before', async () => {
     document.body.innerHTML = '<div id="likeButtonContainer"></div>';
@@ -16,4 +16,35 @@ describe('Liking A Resto', () => {
       document.querySelector('[aria-label="like this resto"]')
     ).toBeTruthy();
   });
+  
+   it('should not show the unlike button when the resto has not been liked before', async () => {
+     document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+     await LikeButtonInitiator.init({
+       likeButtonContainer: document.querySelector('#likeButtonContainer'),
+       restos: {
+         id: 1,
+       },
+     });
+
+     expect(
+       document.querySelector('[aria-label="unlike this resto"]')
+     ).toBeFalsy();
+   });
+
+    it('should be able to like the resto', async () => {
+      document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+      await LikeButtonInitiator.init({
+        likeButtonContainer: document.querySelector('#likeButtonContainer'),
+        restos: {
+          id: 1,
+        },
+      });
+      
+      document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+      const resto = await FavoriteResto.getResto(1);
+      expect(resto).toEqual({ id: 1 });
+      await FavoriteResto.deleteResto(1);
+
+    });
+
 });
