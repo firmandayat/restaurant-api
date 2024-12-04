fe3c473efe94820b48f7c3345fb9dc8439c4ce09
firmandayat <firman.txt@gmail.com>
2024-12-04 19:07:02 +0700

    lazyload

--- sharp.js
+++ sharp.js
@@ -2,6 +2,13 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
+const heros = path.join(__dirname, 'dist', 'images', 'heros');
+
+// Pastikan direktori dibuat
+if (!fs.existsSync(heros)) {
+  fs.mkdirSync(heros, { recursive: true });
+}
+
 const target = path.resolve(__dirname, 'src/public/images/heros');
 const destination = path.resolve(__dirname, 'dist/images/heros');
 
