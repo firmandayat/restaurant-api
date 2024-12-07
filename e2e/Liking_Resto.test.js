1490df030f7a2399362bd8d4a884629999be3ac2
firmandayat <firman.txt@gmail.com>
2024-12-07 14:47:16 +0700

    add end to end testing review resto

--- e2e/Liking_Resto.test.js
+++ e2e/Liking_Resto.test.js
@@ -1,35 +1,35 @@
-// const assert = require('assert');
+const assert = require('assert');
 
-// Feature('Liking Resto');
+Feature('Liking Resto');
 
-// Before(({ I }) => {
-//   I.amOnPage('/#/favorite');
-// });
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
 
-// // Scenario('showing empty favorite resto', ({ I }) => {
-//   //   I.see('Tidak ada resto favorite untuk ditampilkan', '.empty-message');
-//   // });
+// Scenario('showing empty favorite resto', ({ I }) => {
+  //   I.see('Tidak ada resto favorite untuk ditampilkan', '.empty-message');
+  // });
   
-//   // Menambahkan skenario favorite restaurant
-// Scenario('liking one resto', async ({ I }) => {
-//   I.see('Tidak ada resto favorite untuk ditampilkan', '.empty-message');
-//   I.amOnPage('/');
+  // Menambahkan skenario favorite restaurant
+Scenario('liking one resto', async ({ I }) => {
+  I.see('Tidak ada resto favorite untuk ditampilkan', '.empty-message');
+  I.amOnPage('/');
 
-//   // Melihat restaurant dan mengklik untuk detail
-//   I.seeElement('#button');
-//   const firstResto = (locate('#button').first());
-//   const firstRestoTitle = await I.grabTextFrom('.resto__title');
-//   I.click(firstResto);
+  // Melihat restaurant dan mengklik untuk detail
+  I.seeElement('#button');
+  const firstResto = (locate('#button').first());
+  const firstRestoTitle = await I.grabTextFrom('.resto__title');
+  I.click(firstResto);
 
-//   // Melihat tombol like dan mengkliknya
-//   I.seeElement('#likeButton');
-//   I.click('#likeButton');
+  // Melihat tombol like dan mengkliknya
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
-//   // Pergi ke halaman favorite
-//   I.amOnPage('/#/favorite');
-//   I.seeElement('.card');
+  // Pergi ke halaman favorite
+  I.amOnPage('/#/favorite');
+  I.seeElement('.card');
 
-//   // verif like resto
-//   const likedRestoTitle = await I.grabTextFrom('.resto__title');
-//   assert.strictEqual(firstRestoTitle, likedRestoTitle);
-// });
+  // verif like resto
+  const likedRestoTitle = await I.grabTextFrom('.resto__title');
+  assert.strictEqual(firstRestoTitle, likedRestoTitle);
+});
--- e2e/Unliking_Resto.test.js
+++ e2e/Unliking_Resto.test.js
@@ -1,32 +1,32 @@
-// Feature('Unliking Resto');
+Feature('Unliking Resto');
 
-// Before(({ I }) => {
-//   I.amOnPage('/#/favorite');
-// });
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
 
-// // unlike resto
-// Scenario('unliking resto', ({ I }) => {
-//   I.see('Tidak ada resto favorite untuk ditampilkan', '.empty-message');
-//   I.amOnPage('/');
+// unlike resto
+Scenario('unliking resto', ({ I }) => {
+  I.see('Tidak ada resto favorite untuk ditampilkan', '.empty-message');
+  I.amOnPage('/');
 
-//   // Melihat restaurant dan mengklik untuk detail
-//   I.seeElement('#button');
-//   I.click('#button');
+  // Melihat restaurant dan mengklik untuk detail
+  I.seeElement('#button');
+  I.click('#button');
 
-//   // like resto
-//   I.seeElement('#likeButton');
-//   I.click('#likeButton');
+  // like resto
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
-//   // Pergi ke halaman favorite dan klik detail
-//   I.amOnPage('/#/favorite');
-//   I.seeElement('#button');
-//   I.click('#button');
+  // Pergi ke halaman favorite dan klik detail
+  I.amOnPage('/#/favorite');
+  I.seeElement('#button');
+  I.click('#button');
 
-//   // unlike resto
-//   I.seeElement('#likeButton');
-//   I.click('#likeButton');
+  // unlike resto
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
-//   // Pergi ke halaman favorite dan menampilkan Tidak ada resto favorite untuk ditampilkan
-//   I.amOnPage('/#/favorite');
-//   I.see('Tidak ada resto favorite untuk ditampilkan', '.empty-message');
-// });
+  // Pergi ke halaman favorite dan menampilkan Tidak ada resto favorite untuk ditampilkan
+  I.amOnPage('/#/favorite');
+  I.see('Tidak ada resto favorite untuk ditampilkan', '.empty-message');
+});
